43e5fbd09687707c52b5e77f44af1a7340cc589c
wangyanhao <ai_longer@hotmail.com>
2011-06-29 17:23:17 +0800

    first commit

---
+++ springmvctest/src/main/java/com/ciu/web/SpringMvcHello.java
@@ -0,0 +1,19 @@
+package com.ciu.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/helloworld")
+public class SpringMvcHello {
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public ModelAndView hello() {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("helloworld");
+		return mv;
+	}
+	
+}
---
+++ springmvctest/src/main/java/com/ciu/web/interceptor/SpringMvcInterceptor.java
@@ -0,0 +1,36 @@
+package com.ciu.web.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+public class SpringMvcInterceptor extends HandlerInterceptorAdapter {
+
+	@Override
+	public void afterCompletion(HttpServletRequest request,
+			HttpServletResponse response, Object handler, Exception ex)
+			throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("afterCompletion");
+		super.afterCompletion(request, response, handler, ex);
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest request,
+			HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+		System.out.println("postHandle");
+		super.postHandle(request, response, handler, modelAndView);
+	}
+
+	@Override
+	public boolean preHandle(HttpServletRequest request,
+			HttpServletResponse response, Object handler) throws Exception {
+		System.out.println("preHandle");
+		return super.preHandle(request, response, handler);
+	}
+
+	
+}
